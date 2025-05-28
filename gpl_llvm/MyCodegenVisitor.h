ac8764aedf56f57cb7d3a3279c153b43f86d6e55
nash-raf <142609629+nash-raf@users.noreply.github.com>
2025-05-28 07:27:19 +0600

    Update MyCodegenVisitor.h

--- gpl_llvm/MyCodegenVisitor.h
+++ gpl_llvm/MyCodegenVisitor.h
@@ -4,14 +4,13 @@
 #include "generated/BaseBaseVisitor.h"
 #include "generated/BaseParser.h"
 
-#include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
-#include <llvm/IR/Verifier.h>
-
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
+#include <memory>
 #include <string>
 #include <unordered_map>
-#include <memory>
 
 class MyCodegenVisitor : public BaseBaseVisitor {
 public:
@@ -24,7 +23,7 @@ public:
     // Map: variable name -> AllocaInst* (the stack slot)
     std::unordered_map<std::string, llvm::Value*> symbolTable;
 
-    MyCodegenVisitor();   // constructor
+    MyCodegenVisitor();  // Only declare
     ~MyCodegenVisitor() = default;
 
     // Only these methods need to be declared/implemented:
@@ -34,11 +33,14 @@ public:
     antlrcpp::Any visitVarDecl(BaseParser::VarDeclContext *ctx);
     antlrcpp::Any visitAssignmentStatement(BaseParser::AssignmentStatementContext *ctx);
     antlrcpp::Any visitExpr(BaseParser::ExprContext *ctx);
+    antlrcpp::Any visitConditionalStatement(BaseParser::ConditionalStatementContext *ctx);
+
     antlrcpp::Any visitCondition(BaseParser::ConditionContext *ctx);
     antlrcpp::Any visitLoopStatement(BaseParser::LoopStatementContext *ctx);
     antlrcpp::Any visitWhileStatement(BaseParser::WhileStatementContext *ctx);
     antlrcpp::Any visitBlock(BaseParser::BlockContext *ctx);
     // antlrcpp::Any visitPrintStatement(BaseParser::PrintStatementContext *ctx);
+    
 };
 
 #endif // MYCODEGENVISITOR_H

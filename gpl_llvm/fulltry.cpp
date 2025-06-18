419ce08ce9fa83ded55059555152c27e143705b2
nash-raf <142609629+nash-raf@users.noreply.github.com>
2025-06-18 06:44:51 +0600

    Update fulltry.cpp

--- gpl_llvm/fulltry.cpp
+++ gpl_llvm/fulltry.cpp
@@ -404,3 +404,134 @@ public:
 
 
 
+
+
+
+
+
+
+grammar Base;
+
+// Entry point
+program: (statement)* EOF;
+
+GRAPH: 'graph';
+
+// Statements
+statement:
+	loopStatement
+	//| printStatement
+	| varDecl
+	| assignmentStatement
+	| conditionalStatement
+	| graphDef
+	| ';';
+
+// Graph Definition
+
+graphDef: GRAPH graphID '{' nodes? edges? '}' ';';
+nodes: 'nodes:' nodeList ';';
+edges: 'edges:' (edgeList | fileEdgeList) ';';
+nodeList: nodeID (',' nodeID)*;
+edgeList: edge (',' edge)*;
+graphID: ID;
+nodeID: INT;
+fileEdgeList: 'file' STRING;
+edge: nodeID '->' nodeID;
+
+varDecl:
+	type ID ('=' expr)? ';'								# SimpleDeclaration
+	| type arrayDeclarator ('=' arrayInitializer)? ';'	# ArrayDeclaration;
+
+// Array 
+arrayDeclarator:
+	ID '[' INT ']'	# SizedArray
+	| ID '[' ']'	# UnsizedArray;
+
+arrayInitializer: '[' expr (',' expr)* ']'; // Array literal
+
+conditionalStatement:
+	'if' '(' condition ')' block (
+		'else' conditionalStatement
+		| 'else' block
+	)?;
+
+condition:
+	condition AND condition		# LogicalAnd
+	| condition OR condition	# LogicalOr
+	| expr (
+		EQUAL
+		| NOTEQUAL
+		| LESSEQUAL
+		| GREATEREQUAL
+		| LESSTHAN
+		| GREATERTHAN
+	) expr # Relational;
+
+loopStatement: whileStatement;
+whileStatement: 'while' '(' condition ')' block;
+
+assignmentStatement: ID '=' expr ';' | ID ';';
+
+block: '{' (statement)* '}' | '{' '}';
+
+type:
+	'graph'
+	| 'vertex'
+	| 'edge'
+	| 'int'
+	| 'string'
+	| 'real'
+	| 'bool';
+
+// Print printStatement: 'print' printExpr ';' ; printExpr: STRING | expr | printExpr '+' printExpr;
+
+expr:
+	expr (TIMES | DIVIDE) expr	# MulDivExpr
+	| expr (PLUS | MINUS) expr	# AddSubExpr
+	| INT						# IntExpr
+	| ID						# IdExpr
+	| '(' expr ')'				# ParenExpr
+	| ID '[' expr ']'			# ArrayAccessExpr
+	| TRUE						# BoolTrueExpr
+	| FALSE						# BoolFalseExpr
+	| REAL						# RealExpr;
+// | nodeID                	# nodeExpr
+
+// assignment assignmentStatement: ID '=' expr ';' | ID ';';
+
+// op: '==' | '!=' | '<' | '>' | '<=' | '>=' | '||' | '&&'; // Tokens
+EDGE: 'edges';
+NODE: 'nodes';
+TRUE: 'TRUE';
+FALSE: 'FALSE';
+OF: 'of';
+PLUS: '+';
+MINUS: '-';
+TIMES: '*';
+DIVIDE: '/';
+AND: '&&';
+OR: '||';
+EQUAL: '==';
+NOTEQUAL: '!=';
+LESSTHAN: '<';
+GREATERTHAN: '>';
+LESSEQUAL: '<=';
+GREATEREQUAL: '>=';
+
+ID: [a-zA-Z_][a-zA-Z0-9_]*;
+INT: [0-9]+;
+REAL: [0-9]+ '.' [0-9]+;
+//STRING: '"' (~["\r\n])* '"';
+STRING:
+	["] (~["\r\n\\] | '\\' ~[\r\n])* ["]
+	| ['] ( ~['\r\n\\] | '\\' ~[\r\n])* ['];
+
+// Whitespace and Comments COMMENT: '//' ~[\r\n]* -> skip;
+Comment: ( '//' ~[\r\n]* | '/*' .*? '*/') -> skip;
+WS: [ \t\r\n]+ -> skip;
+
+
+
+
+

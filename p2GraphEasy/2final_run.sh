78958e8ca135d5498b532df4546e633be9051f15
nash_raf <niloy.ashraf@northsouth.edu>
2026-03-17 16:44:10 +0600

    Resolve merge conflict

--- p2GraphEasy/2final_run.sh
+++ p2GraphEasy/2final_run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+GRAPH_FILE="${GRAPH_FILE:-test.graph}"
+
 # g++ -c -O2 -std=c++17 graph_loader_runtime.cpp -o graph_loader_runtime.o
 
 g++ -c -O2 -std=c++17 -fopenmp graph_loader_runtime.cpp -o graph_loader_runtime.o
 g++ -c -O2 -std=c++17 graph_runtime.cpp -o graph_runtime.o
 
-./GraphProgram test3.graph
-# ./GraphProgram --debug-polly test.graph
-# ./GraphProgram test.graph -polly-parallel -polly-vectorizer=stripmine
+./GraphProgram "$GRAPH_FILE"
+# ./GraphProgram --debug-polly "$GRAPH_FILE"
+# ./GraphProgram "$GRAPH_FILE" -polly-parallel -polly-vectorizer=stripmine
 
 g++ program.o runtime.o roaring_bitmap.o graph_loader_runtime.o graph_runtime.o -fopenmp -no-pie -o final_program
 export OMP_NUM_THREADS=4
